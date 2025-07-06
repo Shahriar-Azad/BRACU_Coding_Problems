5073ad6f56b4654509947277a04c270385b0512d
Shahriar <shahriar.azad123@gmail.com>
2025-07-06 23:36:20 +0600

    sorting

---
+++ cse110_lab08/sort.py
@@ -0,0 +1,13 @@
+# sort built in function only works with lists. It basically sorts the list in place method..
+
+nums = [1,2,5,33,55,22,4,-9,-3,-5,-2]
+
+nums.sort()
+
+print(nums)
+
+nums = [1,2,5,33,55,22,4,-9,-3,-5,-2]
+
+nums.sort(reverse=True)
+
+print(nums)
