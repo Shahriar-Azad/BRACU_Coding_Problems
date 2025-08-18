d47dc9679cb57e9cd74136ff06484bf354c7770d
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-18 23:24:11 +0600

    add

--- cse110_lab08/s.py
+++ cse110_lab08/s.py
@@ -4,3 +4,18 @@ nums = [7,5,4,2]
 
 for i in range(len(nums)):
     min_index = i
+nums = [7,5,4,2]
+
+for i in range(len(nums)):
+    min_index = i
+
+    for j in range(i+1, len(nums)):
+        if nums[j] < nums[min_index]:
+            min_index = j
+    
+    temp = nums[min_index]
+    nums[min_index] = nums[i]
+    nums[i] = temp
+
+
+print(nums)
