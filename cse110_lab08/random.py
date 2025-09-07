d69d37d01de3e1ecb7493d58f0b0262b816902f0
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-09-08 00:08:17 +0600

    added

--- cse110_lab08/random.py
+++ cse110_lab08/random.py
@@ -2,6 +2,7 @@ def maxProfit(self, prices):
     buy = prices[0]
     profit = 0
     for i in range(1, len(prices)):
-  
+        if prices[i] < buy:
+            buy = prices[i]
      
     return profit
